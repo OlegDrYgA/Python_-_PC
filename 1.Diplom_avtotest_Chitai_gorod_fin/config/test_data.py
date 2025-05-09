1bd99ded0e29fdc6d82b633cb0943c413ca1f57a
olg1967 <olg19671@gmail.comgit config --global user.email>
2025-05-09 12:24:24 +0500

    Диплом по автотестированию Финальный проект Читай город

---
+++ 1.Diplom_avtotest_Chitai_gorod_fin/config/test_data.py
@@ -0,0 +1,5 @@
+class TestData:
+    SEARCH_PHRASE = "гиперболоид инженера гарина"
+    INVALID_SEARCH_PHRASE = "ЫВАФОЛВПДРЖОЬЭПДАЛВЫОФРОВЛАДПРЖОАЛВЫОФ"
+    TEST_PHONE = "9811073543"
+    SPB_CITY = "Санкт-Петербург"
